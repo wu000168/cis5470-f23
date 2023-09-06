40f4929989d1b01a6717bb2d979e254e3d17082c
Z. <wuzed@seas.upenn.edu>
2023-09-06 18:11:44 +0000

    Lab 2

--- lab2/c_programs/test4.c
+++ lab2/c_programs/test4.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-  // TODO: Fill in the code here
+  int a = getchar();
+  if (a == 1) {
+    a = getchar();
+    if (a == 3) {
+      a = getchar();
+      if (a == 5) {
+        a = getchar();
+        if (a == 7) {
+          a = 1 / a;
+        }
+      }
+    }
+  }
   return 0;
 }
