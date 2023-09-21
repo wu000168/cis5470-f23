b908f1b00c46e30b8ac9b4f328b35ab539f56c08
Z. <wuzed@seas.upenn.edu>
2023-09-21 16:13:23 +0000

    Lab 3

--- lab3/src/Fuzzer.cpp
+++ lab3/src/Fuzzer.cpp
@@ -6,32 +6,36 @@
  * implementation, you don't have to modify it.
  */
 
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 
 #include <cstdio>
 #include <cstring>
+#include <iterator>
+#include <numeric>
 #include <string>
+#include <vector>
 
 #include "Utils.h"
 
-#define ARG_EXIST_CHECK(Name, Arg)                                             \
-  {                                                                            \
-    struct stat Buffer;                                                        \
-    if (stat(Arg, &Buffer)) {                                                  \
-      fprintf(stderr, "%s not found\n", Arg);                                  \
-      return 1;                                                                \
-    }                                                                          \
-  }                                                                            \
+#define ARG_EXIST_CHECK(Name, Arg)            \
+  {                                           \
+    struct stat Buffer;                       \
+    if (stat(Arg, &Buffer)) {                 \
+      fprintf(stderr, "%s not found\n", Arg); \
+      return 1;                               \
+    }                                         \
+  }                                           \
   std::string Name(Arg);
 
-#define DBG                                                                    \
+#define DBG \
   std::cout << "Hit F::" << __FILE__ << " ::L" << __LINE__ << std::endl
 
 /**
@@ -77,13 +81,15 @@ std::vector<std::string> PrevCoverageState;
  * @brief Variable to keep track of some Mutation related state.
  * Feel free to change/ignore this if you want to.
  */
-int MutationState = 0;
+int MutationState[] = {1, 1, 1, 1};
+std::vector<int> InputWeights;
 
 /**
  * @brief Variable to keep track of some state related to strategy selection.
  * Feel free to change/ignore this if you want to.
  */
-int StrategyState = 0;
+int StrategyState[] = {1, 2, 5, 2};
+int StrategyStateSize = sizeof(StrategyState) / sizeof(StrategyState[0]);
 
 /************************************************/
 /*    Implement your select input algorithm     */
@@ -101,12 +107,16 @@ int StrategyState = 0;
  * @return Pointer to a string.
  */
 std::string selectInput(RunInfo Info) {
-  int Index = 0;
-  return SeedInputs[Index];
+  int Index =
+      rand() % std::accumulate(InputWeights.begin(), InputWeights.end(), 0);
+  int i = 0; DBG;
+  while (Index >= InputWeights[i]) Index -= InputWeights[i++];
+  
+  return SeedInputs[i];
 }
 
 /*********************************************/
-/*       Implement mutation startegies       */
+/*       Implement mutation strategies       */
 /*********************************************/
 
 const char ALPHA[] = "abcdefghijklmnopqrstuvwxyz\n\0";
@@ -133,11 +143,16 @@ std::string mutationA(std::string Original) { return Original; }
  * @return std::string mutated string.
  */
 std::string mutationB(std::string Original) {
-  if (Original.length() <= 0)
-    return Original;
+  if (Original.length() <= 0) return Original;
 
-  int Index = rand() % Original.length();
-  return Original.insert(Index, 1, ALPHA[rand() % LENGTH_ALPHA]);
+  for (int i = 0; i < MutationState[1];) {
+    int Index = rand() % Original.length();
+    int Length = rand() % std::min(MutationState[1] - i, LENGTH_ALPHA) + 1;
+    Original.insert(Index, Length, ALPHA[rand() % LENGTH_ALPHA]);
+    i += Length;
+  }
+
+  return Original;
 }
 
 /**
@@ -150,13 +165,47 @@ std::string mutationB(std::string Original) {
  */
 
 /**
+ * @brief Mutation Strategy that replaces a random
+ * alpha numeric char at a random location in Original.
+ *
+ * @param Original Original input string.
+ * @return std::string mutated string.
+ */
+std::string mutationC(std::string Original) {
+  if (Original.length() <= 0) return Original;
+
+  for (int i = 0; i < MutationState[2];) {
+    int Index = rand() % Original.length();
+    int Length = rand() % std::min(MutationState[2] - i, LENGTH_ALPHA) + 1;
+    Original.replace(Index, Length, ALPHA[rand() % LENGTH_ALPHA], Length);
+    i += Length;
+  }
+
+  return Original;
+}
+
+std::string mutationD(std::string Original) {
+  if (Original.length() <= 0) return Original;
+
+  for (int i = 0; i < MutationState[3] && Original.length();) {
+    int Index = rand() % Original.length();
+    int Length = rand() % (MutationState[3] - i) + 1;
+    Original.erase(Index, Length);
+    i += Length;
+  }
+
+  return Original;
+}
+
+/**
  * @brief Vector containing all the available mutation functions
  *
  * TODO: Update the definition to include any mutations you implement.
  * For example if you implement mutationC then update it to be:
  * std::vector<MutationFn *> MutationFns = {mutationA, mutationB, mutationC};
  */
-std::vector<MutationFn *> MutationFns = {mutationA, mutationB};
+std::vector<MutationFn *> MutationFns = {mutationA, mutationB, mutationC,
+                                         mutationD};
 
 /**
  * @brief Select a mutation function to apply to the seed input.
@@ -170,9 +219,12 @@ std::vector<MutationFn *> MutationFns = {mutationA, mutationB};
  * @returns a pointer to a MutationFn
  */
 MutationFn *selectMutationFn(RunInfo &Info) {
-  int Strat = rand() % MutationFns.size();
+  int Strat = rand() % std::accumulate(StrategyState,
+                                       StrategyState + StrategyStateSize, 0);
+  int i = 0;
+  while (Strat >= StrategyState[i]) Strat -= StrategyState[i++];
 
-  return MutationFns[Strat];
+  return MutationFns[i];
 }
 
 /*********************************************/
@@ -203,9 +255,37 @@ void feedBack(std::string &Target, RunInfo &Info) {
    * process it (not-necessary). If you do some processing, make sure to update
    * CoverageState to make it available in the next call to feedback.
    */
-  CoverageState.assign(RawCoverageData.begin(),
-                       RawCoverageData.end()); // No extra processing
+  ptrdiff_t lastInput = std::distance(
+      SeedInputs.begin(),
+      std::find(SeedInputs.begin(), SeedInputs.end(), Info.Input));
 
+  ptrdiff_t lastMutation = std::distance(
+      MutationFns.begin(),
+      std::find(MutationFns.begin(), MutationFns.end(), Info.Mutation));
+
+  std::vector<std::string> diff;
+  std::set_difference(RawCoverageData.begin(), RawCoverageData.end(),
+                      PrevCoverageState.begin(), PrevCoverageState.end(),
+                      std::back_inserter(diff));
+
+  if ((MutationState[0] = diff.size()) > 0) {
+    SeedInputs.insert(SeedInputs.end(), Info.MutatedInput);
+    InputWeights.insert(InputWeights.end(), diff.size() * 8);
+  }
+
+  if (!Info.Passed) {
+    StrategyState[lastMutation] += diff.size();
+    InputWeights[lastInput] += diff.size() * 8;
+  } else {
+    if (lastMutation) MutationState[lastMutation] += 1;
+    if (InputWeights[lastInput] > 1) InputWeights[lastInput] -= 1;
+  }
+  
+  CoverageState.assign(RawCoverageData.begin(),
+                       RawCoverageData.end());  // No extra processing
+  // std::set_union(CoverageState.begin(), CoverageState.end(),
+  //                PrevCoverageState.begin(), PrevCoverageState.end(),
+  //                std::back_inserter(CoverageState));
 }
 
 int Freq = 1000;
@@ -226,8 +306,7 @@ bool test(std::string &Target, std::string &Input, std::string &OutDir) {
   fprintf(stderr, "\e[A\rTried %d inputs, %d crashes found\n", Count,
           failureCount);
   if (ReturnCode == 0) {
-    if (PassCount++ % Freq == 0)
-      storePassingInput(Input, OutDir);
+    if (PassCount++ % Freq == 0) storePassingInput(Input, OutDir);
     return true;
   } else {
     storeCrashingInput(Input, OutDir);
@@ -260,9 +339,10 @@ void fuzz(std::string Target, std::string OutDir) {
  */
 int main(int argc, char **argv) {
   if (argc < 4) {
-    printf("usage %s [target] [seed input dir] [output dir] [frequency "
-           "(optional)] [seed (optional arg)]\n",
-           argv[0]);
+    printf(
+        "usage %s [target] [seed input dir] [output dir] [frequency "
+        "(optional)] [seed (optional arg)]\n",
+        argv[0]);
     return 1;
   }
 
@@ -270,8 +350,7 @@ int main(int argc, char **argv) {
   ARG_EXIST_CHECK(SeedInputDir, argv[2]);
   ARG_EXIST_CHECK(OutDir, argv[3]);
 
-  if (argc >= 5)
-    Freq = strtol(argv[4], NULL, 10);
+  if (argc >= 5) Freq = strtol(argv[4], NULL, 10);
 
   int RandomSeed = argc > 5 ? strtol(argv[5], NULL, 10) : (int)time(NULL);
 
@@ -283,6 +362,9 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Cannot read seed input directory\n");
     return 1;
   }
+
+  InputWeights = std::vector<int>(SeedInputs.size(), 32);
+
   fprintf(stderr, "Fuzzing %s...\n\n", Target.c_str());
   fuzz(Target, OutDir);
   return 0;
